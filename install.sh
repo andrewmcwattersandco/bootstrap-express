0f89001e4c8f37f70b0c59c695fa241b19e7c7a9
Andrew McWatters <mcwattersandrew@gmail.com>
2020-11-15 15:01:20 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt update && sudo apt-get upgrade -y
+
+sudo apt install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
+nvm install node
+sudo setcap cap_net_bind_service=+ep `which node`
+
+git config --global credential.helper store
