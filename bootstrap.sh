9f9320341452e3a347a5fc6c960d974bfa0a934b
Andrew McWatters <andrew@andrewmcwatters.com>
2022-04-09 10:24:41 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# Update packages.
-sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get update
 
 # https://github.com/nvm-sh/nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
