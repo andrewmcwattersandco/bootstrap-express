7c971e4a1443ceec80862387944895cb1325a995
Andrew McWatters <andrew@andrewmcwatters.com>
2023-04-28 10:23:26 -0700

    Update nvm to v0.39.3

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ sudo apt-get -y install git
 git config --global credential.helper store
 
 # https://github.com/nvm-sh/nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
