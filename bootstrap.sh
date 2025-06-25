61a45345af2f5d70755bfd5e81fc3816dcb4104e
Andrew McWatters <andrew@andrewmcwatters.com>
2025-06-24 17:07:34 -0700

    Update nvm to v0.40.3

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ sudo apt-get -y install git
 # https://docs.github.com/en/authentication/connecting-to-github-with-ssh/managing-deploy-keys#deploy-keys
 
 # https://github.com/nvm-sh/nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
